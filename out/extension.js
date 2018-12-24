1c52cacb2a3a730a0eb7a86ff620bff63dde4b46
lndgalante <lndgalante@gmail.com>
2018-12-23 22:48:01 -0300

    Improve code

--- out/extension.js
+++ out/extension.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const opn = require('opn');
 const RandExp = require('randexp');
+// Utils
 const isValidRegex = (string) => {
     let isValid = true;
     try {
@@ -15,36 +14,34 @@ const isValidRegex = (string) => {
     }
     return isValid;
 };
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "regex-share" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.shareRegex', () => {
-        // The code you place here will be executed every time your command is executed
         const editor = vscode.window.activeTextEditor;
         const selection = editor.selection;
+        // Get text selected
         const textSelected = editor.document.getText(selection);
         const isRegex = isValidRegex(textSelected);
-        if (!isRegex) {
-            vscode.window.showInformationMessage('Invalid regex selected');
-            return;
-        }
-        vscode.window.showInformationMessage('Opening in RegExr');
-        const [, regex, flags] = textSelected.split('/');
-        const stringMatchingRegex = Array.from({ length: 6 }, () => new RandExp(regex, flags).gen()).join('\n');
+        // Check if regex is valid or not and display a message
+        if (!isRegex)
+            return vscode.window.showErrorMessage('Invalid regex selected');
+        vscode.window.showInformationMessage('Sharing your RegEx in RegExr');
+        // These constants will come from a default configuration
+        const engine = 'js';
+        const tool = 'explain';
+        const maxExampleCases = 6;
+        // Generate example strings that matches the regex
+        const [, regex, flags = ''] = textSelected.split('/');
+        const randexpRegex = Array.from({ length: maxExampleCases }, () => new RandExp(regex, flags).gen());
+        const matchingRegex = randexpRegex.join('\n');
+        // Generate url to open
         const baseUrl = 'https://regexr.com';
-        const url = `${baseUrl}/?expression=${textSelected}&text=${stringMatchingRegex}&tool=explain`;
+        const url = `${baseUrl}/?expression=${textSelected}&text=${matchingRegex}&tool=${tool}&engine=${engine}`;
+        // Open url in user default browser
         opn(url);
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
 const opn = require('opn')
 const RandExp = require('randexp')
 
-const isValidRegex = (string: string) => {
+// Utils
+const isValidRegex = (text: string) => {
   let isValid = true
 
   try {
-    new RegExp(string)
+    new RegExp(text)
   } catch (error) {
     isValid = false
   }
@@ -17,43 +16,38 @@ const isValidRegex = (string: string) => {
   return isValid
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "regex-share" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('extension.shareRegex', () => {
-    // The code you place here will be executed every time your command is executed
-
     const editor = vscode.window.activeTextEditor
     const selection = editor.selection
 
+    // Get text selected
     const textSelected = editor.document.getText(selection)
     const isRegex = isValidRegex(textSelected)
 
-    if (!isRegex) {
-      vscode.window.showInformationMessage('Invalid regex selected')
-      return
-    }
+    // Check if regex is valid or not and display a message
+    if (!isRegex) return vscode.window.showErrorMessage('Invalid regex selected')
+    vscode.window.showInformationMessage('Sharing your RegEx in RegExr')
 
-    vscode.window.showInformationMessage('Opening in RegExr')
+    // These constants will come from a default configuration
+    const engine = 'js'
+    const tool = 'explain'
+    const maxExampleCases = 6
 
-    const [, regex, flags] = textSelected.split('/')
-    const stringMatchingRegex = Array.from({ length: 6 }, () => new RandExp(regex, flags).gen()).join('\n')
+    // Generate example strings that matches the regex
+    const [, regex, flags = ''] = textSelected.split('/')
+    const randexpRegex = Array.from({ length: maxExampleCases }, () => new RandExp(regex, flags).gen())
+    const matchingRegex = randexpRegex.join('\n')
 
+    // Generate url to open
     const baseUrl = 'https://regexr.com'
-    const url = `${baseUrl}/?expression=${textSelected}&text=${stringMatchingRegex}&tool=explain`
+    const url = `${baseUrl}/?expression=${textSelected}&text=${matchingRegex}&tool=${tool}&engine=${engine}`
 
+    // Open url in user default browser
     opn(url)
   })
 
   context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
