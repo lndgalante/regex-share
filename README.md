42be021ee80b60b245e79ed83f33c7c060407492
lndgalante <lndgalante@gmail.com>
2018-12-27 12:04:48 -0300

    Add configuration properties

--- README.md
+++ README.md
@@ -1,33 +1,70 @@
-<h1 align="center">
-Regex Share
-</h1>
+# RegEx Share
 
-## Preview
+### Preview
 
-## Features
+### Installation
 
-- Share your RegEx online in [RegExr](https://regexr.com)
-- Generates example strings that matches your Regex
+Install through VS Code extensions. Search for `RegEx Share`
 
-## How to
+### Features
+
+✅ Visualize your RegEx online in [RegExr](https://regexr.com)
+
+✅ Generates example strings that matches your Regex
+
+### Usage
 
 1. Select your RegEx
 
-2) Press **CMD/CTRL + Shift + R**
+2) Press CMD/CTRL + Shift + R
 
-3. Or press **CMD/CTRL + Shift + P** and find _Share your Regex: Share_
+3. Or using Command Palette (CMD/CTRL + Shift + P) -> Share your RegEx
 
-## To Do
+### RegEx Share Settings
+
+#### regexShare.maxExampleCases (default: 12)
+
+Max number of examples that matches the RegEx
+
+#### regexShare.engine (default: "js")
+
+RegExr currently supports JavaScript RegExp executed in your browser and PCRE via PHP. Valid options:
+
+- "js" - Your browser's JavaScript engine is used to execute RegEx in an asynchronous worker using RegExp.exec()
+
+* "pcre" - PHP 7.0.6 and PCRE 8.38 2015-11-23 are used to execute your pattern on our server
+
+#### regexShare.tool (default: "explain")
+
+Tools provide different ways of working with or exploring your results. Valid options:
+
+- "replace" - The Replace tool replaces matches with a specified string or pattern.
+
+- "list" - The List tool lists all found matches.
+
+- "details" - The Details tool displays the full text of a match and its capture groups.
+
+- "explain" - The Explain tool displays a detailed breakdown of the Expression.
+
+### To Do
 
 - [x] Shorcut to share the Regex i.e CMD + Shift + R
 
-* [ ] Configure how many example strings i.e "regexShare.maxCases": 10. By default is set to 12.
+* [x] Configure how many example strings i.e "regexShare.maxCases": 10. By default is set to 12.
 
-- [ ] Configure what Regexr `tool` to use i.e "regexShare.tool": "explain".
+- [x] Configure what Regexr `tool` to use i.e "regexShare.tool": "explain".
       The tools are: replace, list, details, or explain (default).
 
-* [ ] Configure what Regexr `engine` to use i.e "regexShare.engine": "js".
+* [x] Configure what Regexr `engine` to use i.e "regexShare.engine": "js".
       The engines are: pcre or js (default).
+
+### Related Issues
+
+- [ Generate short URLs via query string parameters](https://github.com/gskinner/regexr/issues/294)
+
+### Contribute
+
+Feel free to open issues or PRs!
 
 ### License
 
--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ const RandExp = require('randexp')
 
 export function activate(context: vscode.ExtensionContext) {
   const disposableShareRegex = vscode.commands.registerCommand('extension.shareRegex', () => {
+    // Destructure VSCode window and messages API
     const { window } = vscode
     const { showErrorMessage, showInformationMessage } = window
 
@@ -21,46 +22,26 @@ export function activate(context: vscode.ExtensionContext) {
     if (!isRegex) return showErrorMessage('Invalid regex selected')
     if (!/^\/(.*?)\//.test(textSelected)) return showErrorMessage('Your RegEx should include slashes, i.e /[0-9]/')
     if (!/^\/(.*?)\/((g|i|m|u|y)*?)$/.test(textSelected)) return showErrorMessage('Invalid RegEx flag found')
-    showInformationMessage('Sharing your RegEx in RegExr')
 
-    // These constants will come from a default configuration
-    const engine = 'js'
-    const tool = 'explain'
-    const maxExampleCases = 12
+    // Get default RegEx Share config
+    const { maxExampleCases, tool, engine } = vscode.workspace.getConfiguration('regexShare')
 
     // Generate example strings that matches the regex
     const [, regex, flags = ''] = textSelected.split('/')
     const randexpRegex = Array.from({ length: maxExampleCases }, () => new RandExp(regex, flags).gen())
     const regexWithoutRepetitions = [...new Set(randexpRegex)]
     const matchingRegex = regexWithoutRepetitions.join('\n')
-    console.log('matchingRegex: ', matchingRegex)
 
-    // Generate url to open
+    // Generate url to be open
     const baseUrl = 'https://regexr.com'
     const url = `${baseUrl}/?expression=${textSelected}&text=${matchingRegex}&tool=${tool}&engine=${engine}`
 
-    // Open url in user default browser
+    // Open url in user default browser and display a message
     opn(url)
+    showInformationMessage('Sharing your RegEx in RegExr')
   })
 
-  const disposableSetMaxExampleCases = vscode.commands.registerCommand('extension.setMaxExampleCases', () => {
-    vscode.window.showInformationMessage('Setting max example cases')
-  })
-
-  const disposableSetRegExrTool = vscode.commands.registerCommand('extension.setRegExrTool', () => {
-    vscode.window.showInformationMessage('Setting RegExr tool')
-  })
-
-  const disposableSetRegExrEngine = vscode.commands.registerCommand('extension.setRegExrEngine', () => {
-    vscode.window.showInformationMessage('Setting RegExr engine')
-  })
-
-  context.subscriptions.push(
-    disposableShareRegex,
-    disposableSetMaxExampleCases,
-    disposableSetRegExrTool,
-    disposableSetRegExrEngine
-  )
+  context.subscriptions.push(disposableShareRegex)
 }
 
 export function deactivate() {}
